e5aaff40197fcf384d2616b0b11dfcc137ef899e
GriefGeek <GriefGeek@yandex.ru>
2022-07-01 02:52:16 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,42 @@ importScripts(
     "https://raw.githubusercontent.com/lodash/lodash/npm-packages/lodash.isequal/index.js",
 );
 
-const service = {
-    isStateEqual: (prev, next) => isEqual(prev, next),
+importScripts(
+    "https://cdn.jsdelivr.net/npm/comlink@4.3.1/dist/umd/comlink.min.js",
+);
+
+let service = {
+    sort: () => {
+        let a = [];
+
+        for (let i = 50000; i >= 0; i--) {
+            a.push(i);
+        }
+
+        function bubbleSort(a) {
+            let swapped;
+            do {
+                swapped = false;
+                for (let i = 0; i < a.length - 1; i++) {
+                    if (a[i] > a[i + 1]) {
+                        let temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        let start = new Date().getTime();
+        bubbleSort(a);
+        let end = new Date().getTime();
+        let time = end - start;
+
+        return time;
+    },
+
+    isStateEqual: (prev, next) => isEqual(prev, next)
 };
 
-expose(service, self);
+Comlink.expose(service, self);
